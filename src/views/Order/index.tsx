22bab807208ae322693a0c77c2fd409998d82c0f
anthonyfolino <folinoo_@hotmail.com>
2023-08-22 18:39:34 +1000

    feat(began orders table, data)

--- src/views/Order/index.tsx
+++ src/views/Order/index.tsx
@@ -1,9 +1,11 @@
+import { OrdersTable } from '../../components/orders-table'
 import './styles.scss'
 
 export const Order = (): JSX.Element => {
     return (
         <div className='container'>
             <h2>Order</h2>
+            <OrdersTable />
         </div>
     )
 }
