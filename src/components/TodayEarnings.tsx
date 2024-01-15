e2d5cd631cff3ffa50bf410b31eeb1e5a2c40f9b
anthonyfolino <folinoo_@hotmail.com>
2024-01-15 19:22:07 +1100

    feat(installed zustand and set up store)

--- src/components/TodayEarnings.tsx
+++ src/components/TodayEarnings.tsx
@@ -10,7 +10,7 @@ export const TodayEarnings = () => {
             data={todayEarnings} 
             index="Dollars" 
             categories={["Dollars"]} 
-            colors={['grey']}
+            colors={['black']}
         />
     </div>
   )
