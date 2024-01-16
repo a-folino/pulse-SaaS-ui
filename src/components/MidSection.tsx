3582018331bb66f23e7e3c1399d752606ede6b62
anthonyfolino <folinoo_@hotmail.com>
2024-01-16 20:19:38 +1100

    feat(added monthly acquisition component)

--- src/components/MidSection.tsx
+++ src/components/MidSection.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SingleCard } from './SingleCard'
+import { MonthlyAcquisition } from './MonthlyAcquisition'
 
 export const MidSection = () => {
   return (
-    <div className="my-10">
+    <div className="flex flex-col gap-10 my-10">
         <SingleCard title="Sales" data={''} />
+        <MonthlyAcquisition />
     </div>
   )
 }
---
+++ src/components/MonthlyAcquisition.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MonthlyAcquisition = () => {
+  return (
+    <div>MonthlyAcquisition</div>
+  )
+}
